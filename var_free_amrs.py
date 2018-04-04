a3408ca13cf5086b17f6761614e571a475bc7173
blodstone <hardy.oei@gmail.com>
2018-04-04 23:00:45 +0100

    Adding filtering for specific type of AMR (only for Proxy Report Dataset).
    There are bugs: output is still empty.

--- var_free_amrs.py
+++ var_free_amrs.py
@@ -28,138 +28,155 @@ from amr_utils import *
 
 
 def create_args_parser():
-	'''Creating arg parser'''
-	
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-f", required=True,
-						type=str, help="AMR file")
-	parser.add_argument('-output_ext', required=False,
-						default='.tf', help="extension of output AMR files (default .tf)")
-	parser.add_argument('-sent_ext', required=False,
-						default='.sent', help="extension of sentences (default .sent)")
-	args = parser.parse_args()
-	
-	return args
-	
+    '''Creating arg parser'''
 
-def single_line_convert(lines):
-	'''Convert AMRs to a single line, ignoring lines that start with "# ::"'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", required=True,
+                        type=str, help="AMR file")
+    parser.add_argument('-output_ext', required=False,
+                        default='.tf', help="extension of output AMR files (default .tf)")
+    parser.add_argument('-sent_ext', required=False,
+                        default='.sent', help="extension of sentences (default .sent)")
+    parser.add_argument('-output_path', required=True, help="Output path")
+    parser.add_argument('-filter', required=False, help="Filtering for specific AMR type (specific for Proxy dataset.)")
+    args = parser.parse_args()
 
-	all_amrs = []
-	cur_amr = []
-	sents = []
-
-	for line in lines:
-		if not line.strip() and cur_amr:
-			cur_amr_line = " ".join(cur_amr)
-			all_amrs.append(cur_amr_line.strip())
-			cur_amr = []
-		elif line.startswith('# ::snt') or line.startswith('# ::tok'):	#match sentence 
-			sent = re.sub('(^# ::(tok|snt))','',line).strip() #remove # ::snt or # ::tok
-			sents.append(sent)
-		elif not line.startswith('#'):
-			cur_amr.append(line.strip())
-
-	if cur_amr:  # file did not end with newline, so add AMR here
-		all_amrs.append(" ".join(cur_amr).strip())
-	
-	assert(len(all_amrs) == len(sents))	#sanity check
-		
-	return all_amrs, sents
+    return args
 
 
-def delete_wiki(f):
-	'''Delete wiki links from AMRs'''
+def single_line_convert(lines, filter_str):
+    '''Convert AMRs to a single line, ignoring lines that start with "# ::"'''
 
-	no_wiki = []
+    all_amrs = []
+    cur_amr = []
+    sents = []
+    skip = False
+    for line in lines:
+        if not skip:
+            if not line.strip() and cur_amr:
+                cur_amr_line = " ".join(cur_amr)
+                all_amrs.append(cur_amr_line.strip())
+                cur_amr = []
+                skip = False
+            elif line.startswith('# ::snt') or line.startswith('# ::tok'):  # match sentence
+                sent = re.sub('(^# ::(tok|snt))', '', line).strip()  # remove # ::snt or # ::tok
+                sents.append(sent)
+            elif not line.startswith('#'):
+                cur_amr.append(line.strip())
+        if 'snt-type ' + filter_str in line:
+            skip = True
 
-	for line in open(f, 'r'):
-		n_line = re.sub(r':wiki "(.*?)"', '', line, 1)
-		n_line = re.sub(':wiki -', '', n_line)
-		no_wiki.append((len(n_line) - len(n_line.lstrip())) * ' ' + ' '.join(n_line.split())) # convert double whitespace but keep leading whitespace
+    if cur_amr:  # file did not end with newline, so add AMR here
+        all_amrs.append(" ".join(cur_amr).strip())
 
-	return no_wiki
+    assert (len(all_amrs) == len(sents))  # sanity check
+
+    return all_amrs, sents
+
+
+def delete_wiki(f, filter_str):
+    '''Delete wiki links from AMRs'''
+
+    no_wiki = []
+    skip = False
+    for line in open(f, 'r'):
+        if not skip:
+            if not line.strip():
+                skip = False
+            n_line = re.sub(r':wiki "(.*?)"', '', line, 1)
+            n_line = re.sub(':wiki -', '', n_line)
+            no_wiki.append((len(n_line) - len(n_line.lstrip())) * ' ' + ' '.join(
+                n_line.split()))  # convert double whitespace but keep leading whitespace
+        if 'snt-type ' + filter_str in line:
+            skip = True
+    return no_wiki
 
 
 def process_var_line(line, var_dict):
-	'''Function that processes line with a variable in it. Returns the string without
-	   variables and the dictionary with var-name + var - value
-	   Only works if AMR is shown as multiple lines and input correctly!'''
+    '''Function that processes line with a variable in it. Returns the string without
+       variables and the dictionary with var-name + var - value
+       Only works if AMR is shown as multiple lines and input correctly!'''
 
-	curr_var_name = False
-	curr_var_value = False
-	var_value = ''
-	var_name = ''
+    curr_var_name = False
+    curr_var_value = False
+    var_value = ''
+    var_name = ''
 
-	for idx, ch in enumerate(line):
-		if ch == '/':				# we start adding the variable value
-			curr_var_value = True
-			curr_var_name = False
-			var_value = ''
-			continue
+    for idx, ch in enumerate(line):
+        if ch == '/':  # we start adding the variable value
+            curr_var_value = True
+            curr_var_name = False
+            var_value = ''
+            continue
 
-		if ch == '(':				# we start adding the variable name
-			curr_var_name = True
-			curr_var_value = False
-			if var_value and var_name:  # we already found a name-value pair, add it now
-				var_dict[var_name.strip()] = var_value.strip().replace(')', '').replace(' :name', '').replace(' :dayperiod', '').replace(' :mod', '')
-			var_name = ''
-			continue
+        if ch == '(':  # we start adding the variable name
+            curr_var_name = True
+            curr_var_value = False
+            if var_value and var_name:  # we already found a name-value pair, add it now
+                var_dict[var_name.strip()] = var_value.strip().replace(')', '').replace(' :name', '').replace(
+                    ' :dayperiod', '').replace(' :mod', '')
+            var_name = ''
+            continue
 
-		if curr_var_name:		# add to variable name
-			var_name += ch
-		if curr_var_value:		# add to variable value
-			var_value += ch
+        if curr_var_name:  # add to variable name
+            var_name += ch
+        if curr_var_value:  # add to variable value
+            var_value += ch
 
-	var_dict[var_name.strip()] = var_value.strip().replace(')', '')
-	deleted_var_string = re.sub(r'\((.*?/)', '(', line).replace('( ', '(')  # delete variables from line
-	
-	return deleted_var_string, var_dict
+    var_dict[var_name.strip()] = var_value.strip().replace(')', '')
+    deleted_var_string = re.sub(r'\((.*?/)', '(', line).replace('( ', '(')  # delete variables from line
+
+    return deleted_var_string, var_dict
 
 
-def delete_amr_variables(amrs):
-	'''Function that deletes variables from AMRs'''
+def delete_amr_variables(amrs, filter_str):
+    '''Function that deletes variables from AMRs'''
 
-	var_dict = dict()
-	del_amr = []
+    var_dict = dict()
+    del_amr = []
+    skip = False
+    for line in amrs:
+        if not skip:
+            if line.strip() and line[0] != '#':
+                if '/' in line:  # variable here
+                    deleted_var_string, var_dict = process_var_line(line, var_dict)  # process line and save variables
+                    del_amr.append(deleted_var_string)  # save string with variables deleted
 
-	for line in amrs:
-		if line.strip() and line[0] != '#':
-		   
-			if '/' in line:		# variable here
-				deleted_var_string, var_dict = process_var_line(line, var_dict) # process line and save variables
-				del_amr.append(deleted_var_string)								# save string with variables deleted
+                else:  # (probable) reference to variable here!
+                    split_line = line.split()
+                    ref_var = split_line[1].replace(')', '')  # get var name
 
-			else:				# (probable) reference to variable here!
-				split_line = line.split()
-				ref_var = split_line[1].replace(')', '')						# get var name
-				
-				if ref_var in var_dict:
-					ref_value = var_dict[ref_var]								# value to replace the variable name with
-					split_line[1] = split_line[1].replace(ref_var, '(' + ref_value.strip() + ')')   # do the replacing and add brackets for alignment
-					n_line = (len(line) - len(line.lstrip())) * ' ' + " ".join(split_line)
-					del_amr.append(n_line)
-				else:
-					del_amr.append(line)  # no reference found, add line without editing (usually there are numbers in this line)
-		else:
-			del_amr.append(line)  # line with other info, just add
-
-	return del_amr
+                    if ref_var in var_dict:
+                        ref_value = var_dict[ref_var]  # value to replace the variable name with
+                        split_line[1] = split_line[1].replace(ref_var,
+                                                              '(' + ref_value.strip() + ')')  # do the replacing and add brackets for alignment
+                        n_line = (len(line) - len(line.lstrip())) * ' ' + " ".join(split_line)
+                        del_amr.append(n_line)
+                    else:
+                        del_amr.append(
+                            line)  # no reference found, add line without editing (usually there are numbers in this line)
+            elif not line.strip():
+                skip = False
+            else:
+                del_amr.append(line)  # line with other info, just add
+        if 'snt-type ' + filter_str in line:
+            skip = True
+    return del_amr
 
 
 if __name__ == "__main__":
-	args = create_args_parser()
-	
-	print 'Converting {0}...'.format(args.f)
-	
-	amr_no_wiki = delete_wiki(args.f)
-	del_amrs = delete_amr_variables(amr_no_wiki)
-	single_amrs, sents = single_line_convert(del_amrs)
+    args = create_args_parser()
 
-	assert len(single_amrs) == len(sents) # sanity check
+    print 'Converting {0}...'.format(args.f)
 
-	out_tf = args.f + args.output_ext
-	out_sent = args.f + args.sent_ext
+    amr_no_wiki = delete_wiki(args.f, args.filter)
+    del_amrs = delete_amr_variables(amr_no_wiki, args.filter)
+    single_amrs, sents = single_line_convert(del_amrs, args.filter)
 
-	write_to_file(single_amrs, out_tf)
-	write_to_file(sents, out_sent)
+    assert len(single_amrs) == len(sents)  # sanity check
+
+    out_tf = os.path.join(args.output_path, args.filter + '_' + os.path.basename(args.f) + args.output_ext)
+    out_sent = os.path.join(args.output_path, args.filter + '_' + os.path.basename(args.f) + args.sent_ext)
+
+    write_to_file(single_amrs, out_tf)
+    write_to_file(sents, out_sent)
